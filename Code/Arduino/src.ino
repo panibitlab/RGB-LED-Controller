a7663c6cd92c7cdf112e41a53f1e7a57de7d0cc8
~*{ Pani }*~ <pigmentviolet19.v@gmail.com>
2026-08-28 12:12:27 +0330

    Updated arduino code with handshake protocol

--- Code/Arduino/src.ino
+++ Code/Arduino/src.ino
@@ -2,8 +2,6 @@ int RED_PIN = 3;
 int GREEN_PIN = 5;
 int BLUE_PIN = 6;
 
-String cmd;
-
 void setup() {
   Serial.begin(9600);
 
@@ -18,23 +16,33 @@ void setup() {
 
 void loop() {
   if (Serial.available()) {
-    cmd = Serial.readStringUntil('\n');
-    cmd.trim();
+    String command = Serial.readStringUntil('\n');
+    command.trim();
 
-    if (cmd.startsWith("COLOR:")) {
-      int first = cmd.indexOf(':');
+    // Connection test
+    if (command == "PING") {Serial.println("PONG");}
+      
+    // RGB color
+    else if (command.startsWith("COLOR:")) {
+      int first = command.indexOf(':');
       String values = cmd.substring(first + 1);
 
       int comma1 = values.indexOf(',');
       int comma2 = values.indexOf(',', comma1 + 1);
 
-      int r = values.substring(0, comma1).toInt();
-      int g = values.substring(comma1 + 1, comma2).toInt();
-      int b = values.substring(comma2 + 1).toInt();
+      if (comma1 != -1 && comma2 != -1) {
+        int r = values.substring(0, comma1).toInt();
+        int g = values.substring(comma1 + 1, comma2).toInt();
+        int b = values.substring(comma2 + 1).toInt();
 
-      analogWrite(RED_PIN, r);
-      analogWrite(GREEN_PIN, g);
-      analogWrite(BLUE_PIN, b);
+        r = constrain(r, 0, 255);
+        g = constrain(g, 0, 255);
+        b = constrain(b, 0, 255);
+        
+        analogWrite(RED_PIN, r);
+        analogWrite(GREEN_PIN, g);
+        analogWrite(BLUE_PIN, b);
+      }
     }
   }
 }
